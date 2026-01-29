20febcd9e948ceea712d1a0921b6465b089b301d
THCS Ngo Si Lien <thcsngosilien.tphcm@gmail.com>
2026-01-29 07:48:00 +0700

    Fix plugin syntax in lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,9 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri::plugin_updater::Builder::new().build()) // Kích hoạt Update
-        .plugin(tauri::plugin_process::init()) // Kích hoạt Process (để restart app)
-        .plugin(tauri::plugin_shell::init())
+        .plugin(tauri_plugin_updater::Builder::new().build()) // Sửa tauri:: -> tauri_plugin_...
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_shell::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
