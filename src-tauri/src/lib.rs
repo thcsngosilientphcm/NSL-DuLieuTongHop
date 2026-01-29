b236d9da9ffe4352ca74721879ac85eb502bb6f4
THCS Ngo Si Lien <thcsngosilien.tphcm@gmail.com>
2026-01-29 08:14:30 +0700

    Add missing opener plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,8 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_updater::Builder::new().build()) // Sửa tauri:: -> tauri_plugin_...
+        .plugin(tauri_plugin_opener::init()) // <-- THÊM DÒNG NÀY
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_shell::init())
         .run(tauri::generate_context!())
